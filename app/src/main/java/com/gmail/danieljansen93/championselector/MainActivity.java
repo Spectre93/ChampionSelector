bf529164b3713dd98f781645c9c0e36d160795da
Daniel Jansen <djaniboe@gmail.com>
2015-10-31 10:47:45 +0100

    Added side select screen
    -Added the layout
    -Added ids to the button handler

--- app/src/main/java/com/gmail/danieljansen93/championselector/MainActivity.java
+++ app/src/main/java/com/gmail/danieljansen93/championselector/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gmail.danieljansen93.championselector;
 
 import android.app.Activity;
+import android.graphics.LightingColorFilter;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,8 +16,18 @@ public class MainActivity extends Activity{
 	public void buttonPressed(View view){
 		switch(view.getId()){
 			case R.id.v3:
+				setContentView(R.layout.side_select);
+				findViewById(R.id.blueSideButton).getBackground().setColorFilter(new LightingColorFilter(0x00000000, 0xFF0000CC));
+				findViewById(R.id.redSideButton).getBackground().setColorFilter(new LightingColorFilter(0x00000000, 0xFFCC0000));
 				break;
 			case R.id.v5:
+				setContentView(R.layout.side_select);
+				findViewById(R.id.blueSideButton).getBackground().setColorFilter(new LightingColorFilter(0x00000000, 0xFF0000CC));
+				findViewById(R.id.redSideButton).getBackground().setColorFilter(new LightingColorFilter(0x00000000, 0xFFCC0000));
+				break;
+			case R.id.redSideButton:
+				break;
+			case R.id.blueSideButton:
 				break;
 
 		}
