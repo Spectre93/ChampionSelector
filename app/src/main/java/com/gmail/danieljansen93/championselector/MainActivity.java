a8aae72e44a91354edaa1b88efd93a80eb7f5664
Daniel Jansen <djaniboe@gmail.com>
2015-10-31 10:44:37 +0100

    Added mode select screen
    -Added buttonPressed handler
    -Added mode_select screen layout

--- app/src/main/java/com/gmail/danieljansen93/championselector/MainActivity.java
+++ app/src/main/java/com/gmail/danieljansen93/championselector/MainActivity.java
@@ -2,6 +2,7 @@ package com.gmail.danieljansen93.championselector;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity{
 
@@ -9,5 +10,15 @@ public class MainActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mode_select);
+	}
+
+	public void buttonPressed(View view){
+		switch(view.getId()){
+			case R.id.v3:
+				break;
+			case R.id.v5:
+				break;
+
+		}
 	}
 }
